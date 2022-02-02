25db84b121eead304633de6e150282bbcda610c7
d4n13ls1104 <78829103+d4n13ls1104@users.noreply.github.com>
2022-02-01 23:59:34 -0500

    Delete src directory

--- src/channels/dto/create-channel-errors.ts
+++
@@ -1,6 +0,0 @@
-import { ApiError } from '../../types/api-error';
-
-export const GENERIC_ERROR: ApiError = {
-  code: 'GENERIC_ERROR',
-  message: 'Something went wrong. Please try again later.',
-};
