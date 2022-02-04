65587a338000b426fe49d069838f59c3b97a2e6f
Andrew Daniels <markassbrownly555@gmail.com>
2022-02-04 16:31:56 -0500

    change name to LinkText because im using reacts link component

--- client/src/components/common/Link.tsx
+++ client/src/components/common/Link.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
-export const Link = styled.a<{
+export const LinkText = styled.span<{
   linkColor?: string | undefined;
   hoverColor?: string | undefined;
 }>`
   color: ${(props) =>
     props.linkColor !== undefined ? props.linkColor : `#4C21E8`};
-  text-decoration: none;
   transition: color 150ms;
 
   :hover {
