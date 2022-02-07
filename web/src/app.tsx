d4e32783cd210300d8f34dbb16c8a9434d665004
Andrew Daniels <markassbrownly555@gmail.com>
2022-02-06 22:18:16 -0500

    add home route and inject auth token into headers for graphql requests

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -3,9 +3,20 @@ import { createClient, Provider } from "urql";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Register } from "./pages/register";
 import { Login } from "./pages/login";
+import { Home } from "./pages/home";
+import { getToken } from "./util/getToken";
 
 const client = createClient({
   url: "http://localhost:3000/graphql",
+  fetchOptions: () => {
+    const token = getToken();
+
+    return {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  },
 });
 
 const App: React.FC = () => {
@@ -13,8 +24,9 @@ const App: React.FC = () => {
     <Provider value={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </Provider>
