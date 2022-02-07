91a883cde552acfa1771e6a2fd8b0da624e8c636
Andrew Daniels <markassbrownly555@gmail.com>
2022-02-06 22:14:46 -0500

    port old components

---
+++ client/src/components/home/UserNav.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const UserNavWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 65px;
+  background-color: #161b22;
+  box-sizing: border-box;
+`;
+
+export const UserNavAvatar = styled.img`
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  margin-left: 10px;
+  align-self: center;
+`;
